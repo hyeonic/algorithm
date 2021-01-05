54cce682c384c035d216a7e1a29f31fdb1076375
hyeonic <evan3566@gmail.com>
2021-01-05 23:25:43 +0900

    feat: Add string in baekjoon
    1316, 2941, 9012, 11719 추가

---
+++ src/main/java/me/hyeonic/algorithm/baekjoon/string/Baekjoon2941.java
@@ -0,0 +1,38 @@
+package me.hyeonic.algorithm.baekjoon.string;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Baekjoon2941 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<String> croatiaAlphabetList = new ArrayList<>();
+
+        croatiaAlphabetList.add("c=");
+        croatiaAlphabetList.add("c-");
+        croatiaAlphabetList.add("dz=");
+        croatiaAlphabetList.add("d-");
+        croatiaAlphabetList.add("lj");
+        croatiaAlphabetList.add("nj");
+        croatiaAlphabetList.add("s=");
+        croatiaAlphabetList.add("z=");
+
+        String word = bufferedReader.readLine();
+
+        for (String croatiaAlphabet : croatiaAlphabetList) {
+            if (word.contains(croatiaAlphabet))
+                word = word.replaceAll(croatiaAlphabet, " ");
+        }
+
+        bufferedWriter.write(String.valueOf(word.length()));
+
+        bufferedWriter.flush();
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
