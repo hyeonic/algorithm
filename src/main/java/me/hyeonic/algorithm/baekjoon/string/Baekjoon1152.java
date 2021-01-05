2793ca567413206180ce66aa7be7ddb1871bbc8c
hyeonic <evan3566@gmail.com>
2021-01-05 21:32:17 +0900

    feat: Add string in baekjoon
    1152, 1157, 8958, 10809, 11721 추가

---
+++ src/main/java/me/hyeonic/algorithm/baekjoon/string/Baekjoon1152.java
@@ -0,0 +1,25 @@
+package me.hyeonic.algorithm.baekjoon.string;
+
+import java.io.*;
+
+public class Baekjoon1152 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] words = bufferedReader.readLine().split(" ");
+
+        int count = 0;
+        for (String word : words) {
+            if (!word.equals("")) ++count;
+        }
+
+        bufferedWriter.write(String.valueOf(count));
+
+        bufferedWriter.flush();
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
