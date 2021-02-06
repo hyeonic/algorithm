08a7a4d421df0c6d5a1465a4f0e76b57f62f03c2
hyeonic <evan3566@gmail.com>
2021-02-06 14:25:24 +0900

    feat: Add string in baekjoon
    string 1212, 10988, 11656

---
+++ src/main/java/me/hyeonic/algorithm/baekjoon/string/Baekjoon11656.java
@@ -0,0 +1,36 @@
+package me.hyeonic.algorithm.baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Baekjoon11656 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String word = bufferedReader.readLine();
+        List<String> dictionary = new ArrayList<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            dictionary.add(word.substring(i));
+        }
+
+        Collections.sort(dictionary);
+
+        for (String s : dictionary) {
+            bufferedWriter.write(s + "\n");
+        }
+
+        bufferedWriter.flush();
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
