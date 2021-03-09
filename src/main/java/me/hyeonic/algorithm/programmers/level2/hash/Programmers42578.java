db0d04cc6f2cc9c50885cbf8eeb88bb1fe20b96b
hyeonic <evan3566@gmail.com>
2021-03-09 23:37:49 +0900

    feat: level2 hash in programmers
    programmers level2 Programmers42577 코드 추가
    programmers level2 Programmers42578

---
+++ src/main/java/me/hyeonic/algorithm/programmers/level2/hash/Programmers42578.java
@@ -0,0 +1,32 @@
+package me.hyeonic.algorithm.programmers.level2.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Programmers42578 {
+
+    public static int solution(String[][] clothes) {
+        int count = 1;
+        Map<String, Integer> clotheMap = new HashMap<>();
+
+        for (String[] clothe : clothes)
+            clotheMap.put(clothe[1], clotheMap.getOrDefault(clothe[1], 0) + 1); // key: 옷 종류 value: 개수
+
+        for (String key : clotheMap.keySet())
+            count *= clotheMap.get(key) + 1; // 안 입는 경우를 포함하기 위해서 +1
+
+        return count - 1; // 모든 옷을 안 입는 경우는 없으므로 -1
+    }
+
+    public static void main(String[] args) {
+
+        String[][][] clothesList = {
+                {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}},
+                {{"crowmask", "face"}, {"bluesunglasses", "face"}, {"smoky_makeup", "face"}}
+        };
+
+        for (int i = 0; i < clothesList.length; i++) {
+            System.out.println(solution(clothesList[i]));
+        }
+    }
+}
