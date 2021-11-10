7c4780e75db147c1a0b861db5efc1d0efdb8cbc0
hyeonic <dev.hyeonic@gmail.com>
2021-11-10 17:39:30 +0900

    feat: Add BOJ10950, 10952 in baekjoon

---
+++ src/main/java/me/hyeonic/algorithm/baekjoon/bronze3/BOJ10950.java
@@ -0,0 +1,19 @@
+package me.hyeonic.algorithm.baekjoon.bronze3;
+
+import java.util.Scanner;
+
+public class BOJ10950 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            System.out.println(a + b);
+        }
+    }
+}
---
+++ src/main/java/me/hyeonic/algorithm/baekjoon/bronze3/BOJ10952.java
@@ -0,0 +1,22 @@
+package me.hyeonic.algorithm.baekjoon.bronze3;
+
+import java.util.Scanner;
+
+public class BOJ10952 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            if (a == 0 && b == 0) {
+                break;
+            }
+
+            System.out.println(a + b);
+        }
+    }
+}
