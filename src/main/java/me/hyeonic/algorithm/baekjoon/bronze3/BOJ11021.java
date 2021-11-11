bfb7fb662d8e5bec4f789295a8488171fe6716a9
hyeonic <dev.hyeonic@gmail.com>
2021-11-11 17:46:36 +0900

    feat: Add BOJ11021 in baekjoon

---
+++ src/main/java/me/hyeonic/algorithm/baekjoon/bronze3/BOJ11021.java
@@ -0,0 +1,19 @@
+package me.hyeonic.algorithm.baekjoon.bronze3;
+
+import java.util.Scanner;
+
+public class BOJ11021 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int t = scanner.nextInt();
+
+        for (int i = 1; i <= t; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            System.out.println("Case #" + i + ": " + (a + b));
+        }
+    }
+}
