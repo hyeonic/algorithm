9f61645ad291e5fb7ad97f10efcec0f1cf486b47
hyeonic <dev.hyeonic@gmail.com>
2021-12-22 01:58:43 +0900

    refactor: BOJ15649, 15650 in baekjoon

--- src/main/java/me/hyeonic/algorithm/baekjoon/silver3/BOJ15649.java
+++ src/main/java/me/hyeonic/algorithm/baekjoon/silver3/BOJ15649.java
@@ -1,32 +1,17 @@
 package me.hyeonic.algorithm.baekjoon.silver3;
 
+import static java.util.stream.Collectors.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class BOJ15649 {
-
-    static int n, m;
-    static int[] result;
-    static boolean[] visited;
-
-    private static void backtracking(int depth) {
-
-        if (depth == m) {
-            for (int i : result) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
-            return;
-        }
-
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                result[depth] = i;
-                backtracking(depth + 1);
-                visited[i] = false;
-            }
-        }
-    }
+    private static int n;
+    private static int m;
+    private static boolean[] visited;
+    private static List<String> results;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -34,9 +19,32 @@ public class BOJ15649 {
         n = scanner.nextInt();
         m = scanner.nextInt();
 
-        result = new int[m];
         visited = new boolean[n + 1];
+        results = new ArrayList<>();
 
-        backtracking(0);
+        backtracking(0, new int[m]);
+
+        results.forEach(System.out::println);
+    }
+
+    private static void backtracking(int depth, int[] values) {
+        if (depth == m) {
+            String result = Arrays.stream(values)
+                .mapToObj(String::valueOf)
+                .collect(joining(" "));
+            results.add(result);
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            visited[i] = true;
+            values[depth] = i;
+            backtracking(depth + 1, values);
+            visited[i] = false;
+        }
     }
 }
--- src/main/java/me/hyeonic/algorithm/baekjoon/silver3/BOJ15650.java
+++ src/main/java/me/hyeonic/algorithm/baekjoon/silver3/BOJ15650.java
@@ -1,32 +1,17 @@
 package me.hyeonic.algorithm.baekjoon.silver3;
 
+import static java.util.stream.Collectors.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class BOJ15650 {
-
-    static int n, m;
-    static int[] result;
-    static boolean[] visited;
-
-    private static void backtracking(int depth, int prev) {
-
-        if (depth == m) {
-            for (int i : result) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
-            return;
-        }
-
-        for (int i = prev; i <= n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                result[depth] = i;
-                backtracking(depth + 1, i + 1);
-                visited[i] = false;
-            }
-        }
-    }
+    private static int n;
+    private static int m;
+    private static boolean[] visited;
+    private static List<String> results;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -34,9 +19,32 @@ public class BOJ15650 {
         n = scanner.nextInt();
         m = scanner.nextInt();
 
-        result = new int[m];
         visited = new boolean[n + 1];
+        results = new ArrayList<>();
 
-        backtracking(0, 1);
+        backtracking(0, 1, new int[m]);
+
+        results.forEach(System.out::println);
+    }
+
+    private static void backtracking(int depth, int start, int[] values) {
+        if (depth == m) {
+            String result = Arrays.stream(values)
+                .mapToObj(String::valueOf)
+                .collect(joining(" "));
+            results.add(result);
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            visited[i] = true;
+            values[depth] = i;
+            backtracking(depth + 1, i, values);
+            visited[i] = false;
+        }
     }
 }
